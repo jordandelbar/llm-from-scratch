abb6e88837a301fd82d36b2671d23c7ef2385b88
Jordan Delbar <jordandelbar@gmail.com>
2025-04-12 22:50:15 +0200

    feat: Use tiktoken as tokenizer

--- Makefile
+++ Makefile
@@ -1,8 +1,10 @@
+.PHONY: precommit-install precommit-run-all run
+
 precommit-install:
-	pre-commit install
+	@pre-commit install
 
 precommit-run-all:
-	pre-commit run --all
+	@pre-commit run --all
 
 run:
-	uv run ./src/main.py
+	@uv run ./src/main.py
--- src/main.py
+++ src/main.py
@@ -1,50 +1,27 @@
-import re
-
 from pathlib import Path
-from typing import Dict, List
 
-from simple_tokenizer import SimpleTokenizerV2
+import tiktoken
 
 
 def main():
-    text = read_the_verdict()
-    preprocessed = simple_tokenizer(text=text)
-    print(len(preprocessed))
-    print(preprocessed[:30])
-    vocab = get_vocab(preprocessed)
-    tokenizer = SimpleTokenizerV2(vocab)
-    text = """"It's the last he painted, you know,"
-               Mrs. Gisburn said with pardonable pride."""
-    ids = tokenizer.encode(text)
-    print(ids)
-    print(tokenizer.decode(ids))
-    text1 = "Hello, do you like tea"
-    text2 = "In the sunlit terraces of the palace."
+    tokenizer = tiktoken.get_encoding("gpt2")
+    text1 = "Hello, do you like tea?"
+    text2 = "In the sunlit terraces of someunknownPlace."
     text = " <|endoftext|> ".join((text1, text2))
-    print(text)
-    print(tokenizer.encode(text))
-    print(tokenizer.decode(tokenizer.encode(text)))
+    integers = tokenizer.encode(text, allowed_special={"<|endoftext|>"})
+    print(integers)
+    string = tokenizer.decode(integers)
+    print(string)
+    text = "Akwirw ier."
+    integers = tokenizer.encode(text, allowed_special={"<|endoftext|>"})
+    string = tokenizer.decode(integers)
+    print(string)
 
 
 def read_the_verdict() -> str:
     with open(f"{Path(__file__).parents[1]}/data/the-verdict.txt") as f:
         text = f.read()
     return text
-
-
-def simple_tokenizer(text: str) -> List[str]:
-    result = re.split(r'([,.:;?_!"()\']|--|\s)', text)
-    result = [item for item in result if item.strip()]
-    return result
-
-
-def get_vocab(preprocessed: List[str]) -> Dict[str, int]:
-    all_tokens = sorted(set(preprocessed))
-    all_tokens.extend(["<|endoftext|>", "<|unk|>"])
-    vocab_size = len(all_tokens)
-    print(f"{vocab_size=}")
-    vocab = {token: integer for integer, token in enumerate(all_tokens)}
-    return vocab
 
 
 if __name__ == "__main__":
