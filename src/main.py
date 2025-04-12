a59a16df47e2b0170a4973a3c06c697a130beda1
Jordan Delbar <jordandelbar@gmail.com>
2025-04-12 23:03:34 +0200

    feat: Data sampling

--- src/main.py
+++ src/main.py
@@ -5,17 +5,25 @@ import tiktoken
 
 def main():
     tokenizer = tiktoken.get_encoding("gpt2")
-    text1 = "Hello, do you like tea?"
-    text2 = "In the sunlit terraces of someunknownPlace."
-    text = " <|endoftext|> ".join((text1, text2))
-    integers = tokenizer.encode(text, allowed_special={"<|endoftext|>"})
-    print(integers)
-    string = tokenizer.decode(integers)
-    print(string)
-    text = "Akwirw ier."
-    integers = tokenizer.encode(text, allowed_special={"<|endoftext|>"})
-    string = tokenizer.decode(integers)
-    print(string)
+    raw_text = read_the_verdict()
+    enc_text = tokenizer.encode(raw_text)
+    print(len(enc_text))
+    enc_sample = enc_text[50:]
+    context_size = 4
+    x = enc_sample[:context_size]
+    y = enc_sample[1 : context_size + 1]
+    print(f"x: {x}")
+    print(f"y:       {y}")
+
+    for i in range(1, context_size + 1):
+        context = enc_sample[:i]
+        desired = enc_sample[i]
+        print(context, "---->", desired)
+
+    for i in range(1, context_size + 1):
+        context = enc_sample[:i]
+        desired = enc_sample[i]
+        print(tokenizer.decode(context), "---->", tokenizer.decode([desired]))
 
 
 def read_the_verdict() -> str:
