62964c91484c1045b4a0420dcb62e2f02c58b6b5
Jordan Delbar <jordandelbar@gmail.com>
2025-04-13 22:22:12 +0200

    feat: Add self attention class with Linear

--- src/main.py
+++ src/main.py
@@ -29,7 +29,7 @@ class GPTDatasetV1(Dataset):
 
 
 class SelfAttention_v1(nn.Module):
-    def __init__(self, d_in, d_out):
+    def __init__(self, d_in, d_out, qkv_bias=False):
         super().__init__()
         self.W_query = nn.Parameter(torch.rand(d_in, d_out))
         self.W_key = nn.Parameter(torch.rand(d_in, d_out))
@@ -39,6 +39,23 @@ class SelfAttention_v1(nn.Module):
         keys = x @ self.W_key
         queries = x @ self.W_query
         values = x @ self.W_value
+        attn_scores = queries @ keys.T
+        attn_weights = torch.softmax(attn_scores / keys.shape[-1] ** 0.5, dim=-1)
+        context_vec = attn_weights @ values
+        return context_vec
+
+
+class SelfAttention_v2(nn.Module):
+    def __init__(self, d_in, d_out, qkv_bias=False):
+        super().__init__()
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+
+    def forward(self, x):
+        keys = self.W_key(x)
+        queries = self.W_query(x)
+        values = self.W_value(x)
         attn_scores = queries @ keys.T
         attn_weights = torch.softmax(attn_scores / keys.shape[-1] ** 0.5, dim=-1)
         context_vec = attn_weights @ values
@@ -94,7 +111,7 @@ def main():
 
 
 def simple_attention_mechanism():
-    torch.manual_seed(123)
+    torch.manual_seed(789)
     inputs = torch.tensor(
         [
             [0.43, 0.15, 0.89],  # Your
@@ -107,8 +124,8 @@ def simple_attention_mechanism():
     )
     d_in = inputs.shape[1]
     d_out = 2
-    sa_v1 = SelfAttention_v1(d_in, d_out)
-    print(sa_v1(inputs))
+    sa_v2 = SelfAttention_v2(d_in, d_out)
+    print(sa_v2(inputs))
 
 
 def read_the_verdict() -> str:
