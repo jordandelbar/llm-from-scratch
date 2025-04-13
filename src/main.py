3ff7fed2b5e5731c779db00a7b89d644da6aa026
Jordan Delbar <jordandelbar@gmail.com>
2025-04-13 11:15:15 +0200

    feat: Attention mechanism for second index

--- src/main.py
+++ src/main.py
@@ -72,6 +72,32 @@ def main():
     print("Positional embeddings shape:\n", pos_embeddings.shape)
     input_embeddings = token_embeddings + pos_embeddings
     print("Input embeddings shape:\n", input_embeddings.shape)
+    simple_attention_mechanism()
+
+
+def simple_attention_mechanism():
+    inputs = torch.tensor(
+        [
+            [0.43, 0.15, 0.89],  # Your
+            [0.55, 0.87, 0.66],  # journey
+            [0.57, 0.85, 0.64],  # starts
+            [0.22, 0.58, 0.33],  # with
+            [0.77, 0.25, 0.10],  # one
+            [0.05, 0.80, 0.55],
+        ]  # step
+    )
+    query = inputs[1]
+    attn_scores_2 = torch.empty(inputs.shape[0])
+    for i, x_i in enumerate(inputs):
+        attn_scores_2[i] = torch.dot(x_i, query)
+    print(attn_scores_2)
+    attn_weights_2 = torch.softmax(attn_scores_2, dim=0)
+    print("Attention wieghts:", attn_weights_2)
+    print("Sum:", attn_weights_2.sum())
+    context_vec_2 = torch.zeros(query.shape)
+    for i, x_i in enumerate(inputs):
+        context_vec_2 += attn_weights_2[i] * x_i
+    print(context_vec_2)
 
 
 def read_the_verdict() -> str:
