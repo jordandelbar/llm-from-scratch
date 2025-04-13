087c2b8a9fca8b304d81bb4cef375d243da71132
Jordan Delbar <jordandelbar@gmail.com>
2025-04-13 10:55:05 +0200

    feat: Token embeddings and positional embeddings

--- src/main.py
+++ src/main.py
@@ -50,15 +50,28 @@ def create_dataloader_v1(
 
 
 def main():
-    vocab_size = 6
-    output_dim = 3
+    vocab_size = 50257
+    output_dim = 256
 
+    raw_text = read_the_verdict()
     torch.manual_seed(123)
-    embedding_layer = torch.nn.Embedding(vocab_size, output_dim)
-    print(embedding_layer.weight)
-    print(embedding_layer(torch.tensor([3])))
-    input_ids = torch.tensor([2, 3, 5, 1])
-    print(embedding_layer(input_ids))
+    token_embedding_layer = torch.nn.Embedding(vocab_size, output_dim)
+    max_length = 4
+    dataloader = create_dataloader_v1(
+        raw_text, batch_size=8, max_length=max_length, stride=max_length, shuffle=False
+    )
+    data_iter = iter(dataloader)
+    inputs, targets = next(data_iter)
+    print("Token IDs:\n", inputs)
+    print("\nInputs shape:\n", inputs.shape)
+    token_embeddings = token_embedding_layer(inputs)
+    print("Tokens embeddings shape:\n", token_embeddings.shape)
+    context_length = max_length
+    pos_embedding_layer = torch.nn.Embedding(context_length, output_dim)
+    pos_embeddings = pos_embedding_layer(torch.arange(context_length))
+    print("Positional embeddings shape:\n", pos_embeddings.shape)
+    input_embeddings = token_embeddings + pos_embeddings
+    print("Input embeddings shape:\n", input_embeddings.shape)
 
 
 def read_the_verdict() -> str:
